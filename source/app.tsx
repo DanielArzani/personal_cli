9c40c568ab7293110cfa67f39953a5120ae386cd
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2023-08-22 08:50:05 -0400

    add notes do gitignore and create switch statement for each script along with a message to go with the chosen script

--- source/app.tsx
+++ source/app.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, Box} from 'ink';
 import SelectInput from 'ink-select-input';
 import {AvailableScripts} from './types/AvailableScriptsType.js';
 
-const items: {label: string; value: AvailableScripts}[] = [
+const scripts: {label: string; value: AvailableScripts}[] = [
 	{label: 'basic-html', value: 'basic-html'},
 	{label: 'sass', value: 'sass'},
 	{label: 'change-dir', value: 'change-dir'},
@@ -16,8 +16,36 @@ type AppProps = {
 };
 
 export default function App({start = 'Starting CLI...'}: AppProps) {
+	const [message, setMessage] = useState<string | null>(null);
+
 	const handleSelect = (item: {label: string; value: AvailableScripts}) => {
-		console.log(item.value);
+		switch (item.value) {
+			case 'basic-html':
+				// Perform action for basic-html
+				setMessage('Creating a basic html,css,js template...');
+
+				break;
+			case 'sass':
+				// Perform action for sass
+				setMessage('Creating a basic sass template...');
+				break;
+			case 'change-dir':
+				// Perform action for change-dir
+				setMessage('Changing directory...');
+				break;
+			case 'create-react-project':
+				// Perform action for create-react-project
+				setMessage('Creating new react project...');
+
+				break;
+			case 'create-react-component':
+				// Perform action for create-react-component
+				setMessage('Creating new react component...');
+
+				break;
+			default:
+				setMessage('Unknown script selected');
+		}
 	};
 
 	return (
@@ -25,10 +53,18 @@ export default function App({start = 'Starting CLI...'}: AppProps) {
 			<Box>
 				<Text>{start}</Text>
 			</Box>
+
 			<Box margin={0}>
 				<Text>Which script would you like to run?</Text>
 			</Box>
-			<SelectInput items={items} onSelect={handleSelect} />
+
+			<Box>
+				<SelectInput items={scripts} onSelect={handleSelect} />
+			</Box>
+
+			<Box>
+				<Text>{message}</Text>
+			</Box>
 		</>
 	);
 }
